61890c4dd358b3ab44b5f515d5bcc7c14deee8f1
Stas Minnebaev <sminnebaev@gmail.com>
2021-01-22 18:13:14 +0300

    Homework 2.2

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,40 @@
-//String.prototype.isPalindrome - для задачи №1
+"use strict"
+// Task 1
+
+String.prototype.isPalindrome = function() {
+    if (this.toLowerCase().replace(/ /g, '') === this.toLowerCase().replace(/ /g, '').split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    let sumMarks = 0;
+    let roundedAverage = 0;
+    if (marks.length !== 0) {
+        for (let i = 0; i < marks.length; i++) {
+            sumMarks += marks[i]; // Сумарное значение оценок
+        }
+
+        roundedAverage = Math.round(sumMarks / marks.length); // Расчет округленного значния средней оценки
+    }
+
+    return roundedAverage;
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = +(new Date());
+    let diff = 0;
+    let age = 0;
+    let verdict = false;
+
+    birthday = new Date(birthday.split('-')[0], birthday.split('-')[1], birthday.split('-')[2]);
+    diff = now - (+birthday);
+    age = diff / 365.25 / 24 / 3600 / 1000; //На лекции Владимир просил проверку на високосный год сделать попроще, поэтому предлагаю к каждому году добавить по 0,25 дня.
+
+    if (age > 18) {
+        verdict = true;
+    }
+    return verdict
 }
